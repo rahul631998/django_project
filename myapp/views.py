524ea7022fd9016e6dc7440bf2eafadeca893d49
rahul631998 <rahulchaudhary631998@gmail.com>
2018-06-26 22:50:48 +0530

    connected api

--- myapp/views.py
+++ myapp/views.py
@@ -12,48 +12,8 @@ from email.mime.text import MIMEText
 import cv2
 import os
 from . import api
-# Create your views here.
-from google_images_download import google_images_download  # importing the library
+from . model import classify as cs
 
-# USAGE
-# python classify.py --model pokedex.model --labelbin lb.pickle --image examples/charmander_counter.png
-
-# import the necessary packages
-# USAGE
-# python classify.py --model pokedex.model --labelbin lb.pickle --image examples/charmander_counter.png
-
-# import the necessary packages
-from keras.preprocessing.image import img_to_array
-from keras.models import load_model
-import numpy as np
-import argparse
-import imutils
-import pickle
-import cv2
-import os
-def prediction (model, labelbin, image):
-    args = [model, labelbin, image]
-    # load the image
-    image = cv2.imread(args[2])
-    # output = image.copy()
-
-    # pre-process the image for classification
-    image = cv2.resize(image, (96, 96))
-    image = image.astype("float") / 255.0
-    image = img_to_array(image)
-    image = np.expand_dims(image, axis=0)
-
-    # load the trained convolutional neural network and the label
-    # binarizer
-    # print("[INFO] loading network...")
-    model = load_model(args[0])
-    lb = pickle.loads(open(args[1], "rb").read())
-    # classify the input image
-    # print("[INFO] classifying image...")
-    proba = model.predict(image)[0]
-    idx = np.argmax(proba)
-    label = lb.classes_[idx]
-    return label
 
 def sendmail(reciever, mes):
     sender = 'nahi.khojoge@gmail.com'  # place your email id here
@@ -97,11 +57,11 @@ def index(request):
         image_predict = new_data.file
         print(str(image_predict))
         new_data.save()
-        result = prediction("myapp/model/new_model", "myapp/model/labelbin", "D:/django_project/django_project/media/"+str(image_predict))
+        result = cs.prediction("myapp/model/new_model", "myapp/model/labelbin", "D:/django_project/django_project/media/"+str(image_predict))
         print(result)
         api.search(result)
     context = {'form': form}
     return render(request, 'index.html', context)
 
 
-def new_index(request):
+
